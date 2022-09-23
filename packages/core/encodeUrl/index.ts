3c32ddb7c374ae51341bbb2936d5dd46b7803663
akashi <akashi_sai@163.com>
2022-09-23 17:52:29 +0800

    :art: fix: lint && typo

--- packages/core/encodeUrl/index.ts
+++ packages/core/encodeUrl/index.ts
@@ -7,26 +7,22 @@ export const encryptUrlParam = (obj: Record<string, string>) => {
 export const decryptUrlParam = (str: string) => {
   const ret = window.decodeURIComponent(window.atob(str))
   return JSON.parse(ret)
-};
+}
 
 export const getUrlQuery = (uri: string) => {
   const url = uri ?? window.location.href
 
-  if (url.indexOf('?') === -1)
+  if (!url.includes('?'))
     return {}
 
-  const queryStr = url.split("?")[1]
-  const list = queryStr.split("&")
+  const queryStr = url.split('?')[1]
+  const list = queryStr.split('&')
 
   let ret: Record<string, string> = {}
-  list.forEach(s => {
+  list.forEach((s) => {
     const [key, value] = s.split('=')
     ret = Object.assign(ret, { [key]: value })
   })
 
-  return ret;
+  return ret
 }
-
-const url = 'https://www.nogi.com/member?age=19&cate=3'
-const ret = getUrlQuery(url)
-encryptUrlParam(ret)
