26081b83806775ec95898c1ef40978fe8abe3555
Akashi <akashi_sai@163.com>
2022-10-19 22:31:46 +0800

    :wrench: chore: lint

--- packages/core/getFileType/index.test.ts
+++ packages/core/getFileType/index.test.ts
@@ -1,8 +1,7 @@
-import { describe ,expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { getFileType } from '.'
 
 describe('should', () => {
-
   it('getFileType', () => {
     const type = 'mp4'
     expect(getFileType('nogi.uta.mp4')).toEqual(type)
