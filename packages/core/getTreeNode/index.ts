494e1390f6b6410ca478c5a957a0abf4f1bb837f
Akashi <akashi_sai@163.com>
2022-10-20 22:46:35 +0800

    :art: feat: getTreeNode

---
+++ packages/core/getTreeNode/index.ts
@@ -0,0 +1,25 @@
+export const getTreeNodes = (tree: any, callback: Function, ret = []) => {
+  tree.forEach((node: any) => {
+    if (callback(node))
+      ret.push(node as never)
+
+    const { children } = node
+    if (Array.isArray(children))
+      ret = [...getTreeNodes(children, callback, ret)]
+  })
+
+  return ret
+}
+
+export const getTreeNode = (tree: any, callback: Function, ret = undefined) => {
+  tree.forEach((node: any) => {
+    if (callback(node))
+      ret = node
+
+    const { children } = node
+    if (Array.isArray(children))
+      ret = getTreeNode(children, callback, ret)
+  })
+
+  return ret
+}
