7ca5edcee1a6068f5203f7d6f4de60a23eab9706
akashi <akashi_sai@163.com>
2023-05-22 11:21:46 +0800

    :sparkles: feat: isPercent && toNumber

--- packages/core/sumPercent/index.md
+++ packages/core/sumPercent/index.md
@@ -9,8 +9,10 @@ Sum percentage.
 ## Usage
 
 ```ts
-import { percentToNumber, sumPercent } from '@use-kit/functions'
+import { isPercent, sumPercent, toNumber } from '@use-kit/functions'
 
-const { x, y } = percentToNumber('20%', '30%') // { x: 20, y: 30 }
+isPercent('15%') // true
+toNumber('15%') // 0.15
+toNumber('15%', false) // 15
 const ret = sumPercent('20%', '13%') // 33%
 ```
--- packages/core/sumPercent/index.ts
+++ packages/core/sumPercent/index.ts
@@ -1,8 +1,30 @@
-export const percentToNumber = (target: string, source: string) => {
+import { isNumber, isString } from '..'
+
+const percentToNumber = (target: string, source: string) => {
   const x = target.split('%')[0]
   const y = source.split('%')[0]
 
   return { x: Number(x), y: Number(y) }
+}
+
+export const isPercent = (target: string | number) => {
+  if (isNumber(target))
+    return false
+
+  return target.toString().includes('%')
+}
+
+export const toNumber = (target: string | number, keepSize = true) => {
+  if (isString(target)) {
+    if (isPercent(target)) {
+      const num = Number((target as string).split('%')[0])
+      return keepSize ? num / 100 : num
+    }
+
+    return Number(target)
+  }
+
+  return target
 }
 
 export const sumPercent = (target: string, source: string) => {
