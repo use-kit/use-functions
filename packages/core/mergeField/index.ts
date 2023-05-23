3d18b29c24364ee72763e978a5395849b66b7d5a
akashi <akashi_sai@163.com>
2023-05-23 18:21:06 +0800

    :art: feat: withDefaultObject

--- packages/core/mergeField/index.ts
+++ packages/core/mergeField/index.ts
@@ -1,1 +1,9 @@
-// TODO:
+interface objParams {
+  [key: string]: any
+}
+
+export const mergeField = (target: objParams, source: objParams) => {
+  Object.keys(source).forEach((sKey) => {
+    target[sKey] = source[sKey]
+  })
+}
