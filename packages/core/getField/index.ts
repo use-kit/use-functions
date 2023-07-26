9b84ae7a2878b43c612a732b795be8e1ac026e69
akashi <akashi_sai@163.com>
2023-07-26 10:01:22 +0800

    :zap: chore: get-field use like lodash.get

--- packages/core/getField/index.ts
+++ packages/core/getField/index.ts
@@ -1,12 +1,24 @@
-export const getField = (
-  target: Record<string, any>,
-  field: string,
-  preset: unknown = undefined,
-) => {
-  // TODO: use like lodash.get()
+// export const getField = (
+//   target: Record<string, any>,
+//   field: string,
+//   preset: unknown = undefined,
+// ) => {
+//   if (Reflect.has(target, field))
+//     return target[field]
 
-  if (Reflect.has(target, field))
-    return target[field]
+//   return preset
+// }
 
-  return preset
+export const getField = <T>(target: any, path: string, preset?: T): T => {
+  const keys = path.split('.')
+  let value: any = target
+
+  for (const key of keys) {
+    if (value === null || value === undefined)
+      return preset!
+
+    value = value[key]
+  }
+
+  return value !== undefined ? value : preset
 }
