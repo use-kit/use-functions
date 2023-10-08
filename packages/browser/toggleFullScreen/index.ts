d12fa0b2b9f52a7ea97f7e04326748a476a80d27
akashi <akashi_sai@163.com>
2023-10-08 17:49:36 +0800

    :zap: chore: remove outdated compatibility

--- packages/browser/toggleFullScreen/index.ts
+++ packages/browser/toggleFullScreen/index.ts
@@ -5,87 +5,33 @@ export function fullScreen(id?: string) {
     : document.documentElement
 
   const screen = (el as Element).requestFullscreen
-  let wScript = null
-  if (typeof screen != 'undefined') {
+
+  if (typeof screen != 'undefined')
     screen.call(el)
-    return
-  }
-  if (typeof window.ActiveXObject != 'undefined') {
-    wScript = new ActiveXObject('WScript.Shell')
-    if (wScript)
-      wScript.SendKeys('{F11}')
-  }
 }
 
 // 退出全屏
 export function exitFullScreen() {
   const screen = document.exitFullscreen
 
-  let wScript = null
-
-  if (typeof screen != 'undefined') {
+  if (typeof screen != 'undefined')
     screen.call(document)
-    return
-  }
-
-  if (typeof window.ActiveXObject != 'undefined') {
-    wScript = new ActiveXObject('WScript.Shell')
-    if (wScript != null)
-      wScript.SendKeys('{F11}')
-  }
 }
 
 export function isFullScreen(): boolean {
-  const fullscreenElement
-    = document.fullscreenElement
+  const element = document.fullscreenElement
     || (document as any).webkitFullscreenElement
     || (document as any).mozFullScreenElement
-  return fullscreenElement !== undefined
+
+  return element !== undefined
 }
 
 export function toggleFullScreen(element?: HTMLElement) {
   element = element || document.documentElement
-  if (
-    (
-      (document as any).fullScreenElement !== undefined
-      && (document as any).fullScreenElement === null
-    )
-    || (
-      (document as any).msFullscreenElement !== undefined
-      && (document as any).msFullscreenElement === null
-    )
-    || (
-      (document as any).mozFullScreen !== undefined
-      && !(document as any).mozFullScreen
-    )
-    || (
-      (document as any).webkitIsFullScreen !== undefined
-      && !(document as any).webkitIsFullScreen
-    )
-  ) {
-    if ((element as any).requestFullScreen)
-      (element as any).requestFullScreen()
 
-    else if ((element as any).mozRequestFullScreen)
-      (element as any).mozRequestFullScreen()
+  if (isFullScreen())
+    exitFullScreen()
 
-    else if ((element as any).webkitRequestFullScreen)
-      (element as any).webkitRequestFullScreen((Element as any).ALLOW_KEYBOARD_INPUT)
-
-    else if ((element as any).msRequestFullscreen)
-      (element as any).msRequestFullscreen()
-  }
-  else {
-    if ((document as any).cancelFullScreen)
-      (document as any).cancelFullScreen()
-
-    else if ((document as any).mozCancelFullScreen)
-      (document as any).mozCancelFullScreen()
-
-    else if ((document as any).webkitCancelFullScreen)
-      (document as any).webkitCancelFullScreen()
-
-    else if ((document as any).msExitFullscreen)
-      (document as any).msExitFullscreen()
-  }
+  else
+    fullScreen(element.id)
 }
